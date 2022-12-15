ef14761a13617c62a7751242c009ce5322b378fe
Larissa Pereira <larissa.pereira@conecthus.org.br>
2022-12-15 16:40:32 -0400

    :tada: nu codelab in the area

---
+++ app/src/main/java/com/alura/travelagency/ui/activity/TravelPackagesActivity.java
@@ -0,0 +1,17 @@
+package com.alura.travelagency.ui.activity
+        ;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alura.travelagency.R;
+
+public class TravelPackagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_packages);
+    }
+}
